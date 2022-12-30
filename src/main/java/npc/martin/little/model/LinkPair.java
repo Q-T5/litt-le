c1f25a56c82257a06686fb5c6ff032769eeb501e
Q-T5 <bikathimartin@outlook.com>
2022-12-30 11:11:17 +0300

    Update LinkPair entity structure

--- src/main/java/npc/martin/little/model/LinkPair.java
+++ src/main/java/npc/martin/little/model/LinkPair.java
@@ -24,8 +24,11 @@ public class LinkPair implements Serializable {
     @Column(name = "original_url")
     private String originalUrl;
     
-    @Column(name = "shor_url")
+    @Column(name = "short_url")
     private String shortUrl;
+    
+    @Column(name = "link_id")
+    private String linkId;
 
     public Long getId() {
         return id;
@@ -51,21 +54,32 @@ public class LinkPair implements Serializable {
         this.shortUrl = shortUrl;
     }
 
-    public LinkPair(Long id, String originalUrl, String shortUrl) {
+    public String getLinkId() {
+        return linkId;
+    }
+
+    public void setLinkId(String linkId) {
+        this.linkId = linkId;
+    }
+
+    public LinkPair(Long id, String originalUrl, String shortUrl, String linkId) {
         this.id = id;
         this.originalUrl = originalUrl;
         this.shortUrl = shortUrl;
+        this.linkId = linkId;
     }
-    
-    public LinkPair(String originalUrl, String shortUrl) {
+
+    public LinkPair(String originalUrl, String shortUrl, String linkId) {
         this.originalUrl = originalUrl;
         this.shortUrl = shortUrl;
+        this.linkId = linkId;
     }
     
     public LinkPair() {}
 
     @Override
     public String toString() {
-        return "LinkPair{" + "id=" + id + ", originalUrl=" + originalUrl + ", shortUrl=" + shortUrl + '}';
+        return "LinkPair{" + "id=" + id + ", originalUrl=" + originalUrl 
+            + ", shortUrl=" + shortUrl + ", linkId=" + linkId + '}';
     }
 }
