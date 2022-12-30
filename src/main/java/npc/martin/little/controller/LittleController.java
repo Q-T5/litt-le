ebfb80309776baa1db2b761419c8df3413dffd7d
Q-T5 <bikathimartin@outlook.com>
2022-12-30 11:02:38 +0300

    Endpoint to shorten URL

--- src/main/java/npc/martin/little/controller/LittleController.java
+++ src/main/java/npc/martin/little/controller/LittleController.java
@@ -1,8 +1,20 @@
 package npc.martin.little.controller;
 
+import npc.martin.little.model.LinkPair;
+import npc.martin.little.payload.request.ShortenRequest;
+import npc.martin.little.service.ConvenienceService;
+import npc.martin.little.service.LinkPairService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  *
@@ -10,10 +22,38 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/api/messages")
+@RequestMapping(value = "/r")
 public class LittleController {
-    @GetMapping(value = "/hello")
-    public String helloWorld() {
-        return "Running VueJS in Spring Boot";
+    @Autowired
+    private LinkPairService linkPairService;
+    
+    @Autowired
+    private ConvenienceService convenienceService;
+    
+    private final Logger logger = LoggerFactory.getLogger(LinkPairService.class);
+    
+    @GetMapping(value = "/shorten")
+    public ResponseEntity<?> shortenUrl(@RequestBody ShortenRequest shortenRequest) {
+        LinkPair newPair = new LinkPair(
+            shortenRequest.getOriginalUrl(), null);
+        
+        String shortenedUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path(String.format("/r/%s", convenienceService.generateUniqueId()))
+            .toString();
+        
+        newPair.setShortUrl(shortenedUrl);
+        try {
+            linkPairService.createShortenedUrl(newPair);
+            return new ResponseEntity(shortenedUrl, HttpStatus.OK);
+        } catch(Exception ex) {
+            logger.error("Encountered error while shortening URL");
+            return new ResponseEntity(
+                "Encountered error while shortening URL", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GetMapping(value = "/{linkId}")
+    public ResponseEntity<?> reverseShortening(@PathVariable String linkId) {
+        
     }
 }
