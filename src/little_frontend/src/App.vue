755fc905076b1b828107049518e6b7667896ea47
Q-T5 <bikathimartin@outlook.com>
2022-12-30 14:43:32 +0300

    Create general view layout

--- src/little_frontend/src/App.vue
+++ src/little_frontend/src/App.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div class="w-[40rem] h-96 rounded-md p-2 shadow-md shadow-gray-500">
+      <div class="w-full h-[10%] flex items-center">
+        <h1>Litt-le URL Shortener</h1>
+      </div>
+      <div class="w-full h-[90%] flex flex-col justify-center items-center">
+        <div class="flex flex-col w-full space-y-1">
+          <h1>Enter Original URL</h1>
+          <input type="text" placeholder="e.g https://www.google.com?..." class="inputbox-styles" />
+          <div class="w-full flex justify-center">
+            <button class="btn-style">Shorten URL</button>
+          </div>
+        </div>
+        <div class="flex w-full flex-col">
+          <h1>Shortened URL</h1>
+          <input type="text" placeholder="auto-generated" class="inputbox-styles border-gray-500" disabled="true" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,20 +27,19 @@ import { ref } from 'vue'
 export default {
   name: "App",
   setup: function() {
-    const msg = ref('');
+    // reactive data
+    const shortenUrlRequest = ref({
+      originalUrl: ''
+    });
+
+    // methods
+    function shortenUrl() {
+
+    }
 
     return {
-      msg
+      shortenUrlRequest, shortenUrl
     }
-  },
-  mounted: function() {
-    fetch("/api/messages/hello", {
-      method: "GET"
-    }).then((response) => {
-      return response.text();
-    }).then((data) => {
-      this.msg = data;
-    })
   }
 }
 </script>
